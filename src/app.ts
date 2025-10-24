8790599c65b1f79fd8a0dceaebfa61a18a98c5e3
samson onifade <gbemilekeadeboyega@gmail.com>
2025-10-24 18:11:28 +0100

    fix cors issus

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,28 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN?.split(',').map((s) => s.trim()) || '*',
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN?.split(',').map((s) => s.trim()) || '*',
+//     credentials: true,
+//   })
+// );
+
+app.use(cors())
+app.disable("x-powered-by");
+console.log(process.env.CORS_ORIGIN, [
+  'http://localhost:3000',
+  'http://localhost:3001',
+])
+
+
+app.use( 
+  cors({ 
+    origin: process.env.CORS_ORIGIN,  
+
     credentials: true,
-  })
-);
+  }),
+)
 
 app.use('/health', healthRouter);
 app.use('/quiz-results', quizResultsRouter);
